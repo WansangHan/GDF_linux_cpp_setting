1ae57a9dbea41c46509086f834c4806c6c7f6614
wshan <wshan@nm-gurobal.com>
2022-07-31 22:48:08 +0900

    windows 프로젝트 추가

---
+++ GDF_linux_cpp_setting/GDF_linux_cpp_setting.cpp
@@ -0,0 +1,12 @@
+﻿// GDF_linux_cpp_setting.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "GDF_linux_cpp_setting.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
